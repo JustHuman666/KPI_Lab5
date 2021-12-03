f41595cf5510a9b4786e1126fbe0ec2cad5d04a2
JustHuman666 <e.myhalchuk@gmail.com>
2021-12-03 15:44:01 +0200

    coments deketed

--- PajeObject.py
+++ PajeObject.py
@@ -2,16 +2,16 @@ from BaseApp import BasePage
 from selenium.webdriver.common.by import By
 
 class YakabooLocators:
-    LOCATOR_YAKABOO_SEARCH_FIELD = (By.ID, 'search')
-    LOCATOR_YAKABOO_GET_RESPONSE_FOR_WRONG = (By.CLASS_NAME, 'note-msg')
-    LOCATOR_YAKABOO_LINK_BUTTON = (By.LINK_TEXT, 'Акції')
-    LOCATOR_YAKABOO_GET_RESPONSE_FOR_LINK = (By.CLASS_NAME, 'blog')
+    SEARCH_FIELD = (By.ID, 'search')
+    GET_RESPONSE_FOR_WRONG = (By.CLASS_NAME, 'note-msg')
+    LINK_BUTTON = (By.LINK_TEXT, 'Акції')
+    GET_RESPONSE_FOR_LINK = (By.CLASS_NAME, 'blog')
 
 
 class RequestHelper(BasePage):
 
     def enter_word(self, word):
-        search_field = self.find_element(YakabooLocators.LOCATOR_YAKABOO_SEARCH_FIELD)
+        search_field = self.find_element(YakabooLocators.SEARCH_FIELD)
         search_field.send_keys(word)
         return search_field
 
@@ -22,12 +22,12 @@ class RequestHelper(BasePage):
         return search_field.click()
 
     def get_error_response(self):
-        return self.find_element(YakabooLocators.LOCATOR_YAKABOO_GET_RESPONSE_FOR_WRONG).text
+        return self.find_element(YakabooLocators.GET_RESPONSE_FOR_WRONG).text
 
     def find_link_by_name(self):
-        return self.find_element(YakabooLocators.LOCATOR_YAKABOO_LINK_BUTTON)
+        return self.find_element(YakabooLocators.LINK_BUTTON)
 
     def get_response_for_clicked_link(self):
-        return self.find_elements(YakabooLocators.LOCATOR_YAKABOO_GET_RESPONSE_FOR_LINK)[0].text
+        return self.find_elements(YakabooLocators.GET_RESPONSE_FOR_LINK)[0].text
 
     
